1d7398fbc6eaab3d57666d306a11909e1422f54b
Joanna Grobelak <joannagrobelak@wp.pl>
2018-06-15 12:15:04 +0200

    dodanie .gitignore

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,34 +6,5 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator(6,4);
-
-        int result2 = calculator.add(6, 4);
-        int result3 = calculator.subtract(6,4);
-        int valueA = calculator.getA();
-        int valueB = calculator.getB();
-
-        if(result2 == valueA + valueB) {
-            System.out.println("test ok!");
-        } else {
-            System.out.println("error!");
-        }
-
-        if(result3 == valueA - valueB) {
-            System.out.println("test ok!");
-        } else {
-            System.out.println("error!");
-        }
-
     }
 }

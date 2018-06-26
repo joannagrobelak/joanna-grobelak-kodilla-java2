06d19a5aa2afbd161354fa19552bb4a00d31c9c9
Joanna Grobelak <joannagrobelak@wp.pl>
2018-06-26 14:20:29 +0200

    zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,27 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.ForumUser;
+import com.kodilla.stream.forumuser.Forum;
+
+import java.time.LocalDate;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
-    public static void main(String[] args) {
-        BookDirectory theBookDirectory = new BookDirectory();
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
 
-        System.out.println(theResultStringOfBooks);
+    public static void main(String[] args) {
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> resultMapOfUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.of(1998, 6,26)))
+                .filter(forumUser -> forumUser.getPostsNumber() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+
+        System.out.println("Number of users after filtration: " + resultMapOfUsers.size());
+        System.out.println("List of users after filtration: ");
+        resultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+    public Forum() {
+
+        forumUserList.add(new ForumUser(1,"Winnie the Pooh", 'M', LocalDate.of(1926, 12, 10), 5670));
+        forumUserList.add(new ForumUser(2, "Piglet", 'M', LocalDate.of(1930, 4, 12), 3500));
+        forumUserList.add(new ForumUser(3, "Tigger", 'M', LocalDate.of(1926, 6, 26), 2788));
+        forumUserList.add(new ForumUser(4, "Kanga", 'F', LocalDate.of(1928, 5, 21), 1434));
+        forumUserList.add(new ForumUser(5, "Roo", 'M', LocalDate.of(2006, 2, 7), 24));
+        forumUserList.add(new ForumUser(6, "Rabbit", 'M', LocalDate.of(1920, 4, 24), 4002));
+        forumUserList.add(new ForumUser(7, "Eeyore", 'M', LocalDate.of(1928, 4, 20), 0));
+
+    }
+
+    public List<ForumUser> getUserList() {
+        return new ArrayList<>(forumUserList);
+    }
+}

95087eac79675afb8b1e9a310f2826b5aa25293e
Joanna Grobelak <joannagrobelak@wp.pl>
2018-07-05 10:36:23 +0200

    zadanie 9.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MainMovieStore.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MainMovieStore {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        String titles = movieStore.getMovies().entrySet().stream()
+                .flatMap(titlesAndTranslations -> titlesAndTranslations.getValue().stream())
+                .collect(Collectors.joining("! "));
+
+        System.out.println(titles);
+
+    }
+}

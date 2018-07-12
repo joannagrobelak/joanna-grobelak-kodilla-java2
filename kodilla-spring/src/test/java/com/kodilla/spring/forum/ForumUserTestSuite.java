713d8f0ac3e33cf8ae2efbb2346807de1b2a7fb1
Joanna Grobelak <joannagrobelak@wp.pl>
2018-07-12 13:43:39 +0200

    zadanie 10.1

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,4 +1,24 @@
 package com.kodilla.spring.forum;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@RunWith(org.springframework.test.context.junit4.SpringRunner.class)
+@SpringBootTest
+
 public class ForumUserTestSuite {
+    @Test
+    public void testGetUserName() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String userName = forumUser.getUserName();
+        //Then
+        Assert.assertEquals("John Smith", userName);
+    }
 }

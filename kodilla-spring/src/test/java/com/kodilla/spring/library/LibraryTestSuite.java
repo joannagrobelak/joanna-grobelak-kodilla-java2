796abd242dbcab6df6fa093396f4a1fdffa1de5d
Joanna Grobelak <joannagrobelak@wp.pl>
2018-07-13 11:59:12 +0200

    poprawki

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -5,9 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(org.springframework.test.context.junit4.SpringRunner.class)
 @SpringBootTest
 public class LibraryTestSuite {
     @Test
